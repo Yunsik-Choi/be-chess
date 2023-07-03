41e09ecbca7a1778aef0cc03dce79c7e749d2c6e
Yunsik-Choi <choiys0311@gmail.com>
2023-07-03 17:55:01 +0900

    ⛑️ test : 폰의 색상을 가져오는 기능 테스트 작성

--- src/test/java/softeer2nd/domain/PawnTest.java
+++ src/test/java/softeer2nd/domain/PawnTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("폰 관련 기능")
 public class PawnTest {
@@ -14,5 +16,14 @@ public class PawnTest {
         Pawn pawn = new Pawn("white");
 
         assertThat(pawn.getColor()).isEqualTo("white");
+    }
+
+    @DisplayName("폰의 색상을 가져온다.")
+    @ParameterizedTest(name = "color : {0}")
+    @ValueSource(strings = {"white", "black"})
+    void getColor(final String color) {
+        Pawn pawn = new Pawn(color);
+
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }
