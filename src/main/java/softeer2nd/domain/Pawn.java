82da59bd2de086b7e079812b3e0fed391bda12e4
Yunsik-Choi <choiys0311@gmail.com>
2023-07-03 17:53:35 +0900

    ⚡ feat : 폰 클래스 구현

---
+++ src/main/java/softeer2nd/domain/Pawn.java
@@ -0,0 +1,28 @@
+package softeer2nd.domain;
+
+import java.util.Objects;
+
+public class Pawn {
+    private final String color;
+
+    public Pawn(final String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Pawn pawn = (Pawn) o;
+        return Objects.equals(color, pawn.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color);
+    }
+}
