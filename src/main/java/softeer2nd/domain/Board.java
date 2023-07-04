d18814e890ad7c4db5ca0151e939ab0add4e45c1
Yunsik-Choi <choiys0311@gmail.com>
2023-07-04 10:36:57 +0900

    ⚡feat : 체스 판에 Pawn 추가하는 기능 구현

---
+++ src/main/java/softeer2nd/domain/Board.java
@@ -0,0 +1,12 @@
+package softeer2nd.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    private final List<Pawn> pawns = new ArrayList<>();
+
+    public void add(final Pawn pawn) {
+        pawns.add(pawn);
+    }
+}
