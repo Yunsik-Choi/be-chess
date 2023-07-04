a0be4f53563722a7b3c92bdc5b93cb3bffe097b0
Yunsik-Choi <choiys0311@gmail.com>
2023-07-04 09:39:58 +0900

    ⚡feat : 폰 기본생성자 기능 구현

--- src/main/java/softeer2nd/domain/Pawn.java
+++ src/main/java/softeer2nd/domain/Pawn.java
@@ -5,6 +5,10 @@ import java.util.Objects;
 public class Pawn {
     private final String color;
 
+    public Pawn() {
+        this("white");
+    }
+
     public Pawn(final String color) {
         this.color = color;
     }
