617013ce3edf14c5b08d428f88d21a705600fb29
Yunsik-Choi <choiys0311@gmail.com>
2023-07-04 10:35:16 +0900

    ⛑️ test : 체스 판에서 Pawn을 추가된 순번으로 가져오는 기능 테스트 작성

--- src/test/java/softeer2nd/domain/BoardTest.java
+++ src/test/java/softeer2nd/domain/BoardTest.java
@@ -3,6 +3,7 @@ package softeer2nd.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,11 +16,11 @@ public class BoardTest {
 
         Pawn white = new Pawn(Pawn.WHITE_COLOR);
         board.add(white);
-        verifyAddPawn(board, white);
+        verifyAddPawn(board, 1, white, 0);
 
         Pawn black = new Pawn(Pawn.BLACK_COLOR);
         board.add(black);
-        verifyAddPawn(board, black);
+        verifyAddPawn(board, 2, black, 1);
     }
 
     @DisplayName("체스 판에 Pawn을 추가한다.")
@@ -31,6 +32,22 @@ public class BoardTest {
         board.add(white);
 
         verifyAddPawn(board, 1, white, 0);
+    }
+
+    @DisplayName("체스 판에서 Pawn을 추가된 순번으로 가져온다.")
+    @Test
+    public void findPawn() {
+        Board board = new Board();
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+
+        board.add(white);
+        board.add(white);
+
+        assertAll(
+                () -> Assertions.assertThat(board.findPawn(0)).isEqualTo(white),
+                () -> Assertions.assertThat(board.findPawn(1)).isEqualTo(black)
+        );
     }
 
     private void verifyAddPawn(final Board board, final int boardSize, final Pawn pawn, final int pawnIndex) {
