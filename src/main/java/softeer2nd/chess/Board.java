cf265e44418636c72d54cc7d6f0c07c48da3a46a
Yunsik-Choi <choiys0311@gmail.com>
2023-07-04 13:36:10 +0900

    ⚡feat : 체스판 초기화 기능 구현

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -3,13 +3,22 @@ package softeer2nd.chess;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import softeer2nd.chess.pieces.Pawn;
 
 public class Board {
+    private static final int WIDTH_SIZE = 8;
+
     private final List<Pawn> pawns = new ArrayList<>();
 
     public void initialize() {
+        initializePawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        initializePawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+    }
 
+    private void initializePawn(final String color, final String representation) {
+        IntStream.range(0, WIDTH_SIZE)
+                .forEach(i -> pawns.add(new Pawn(color, representation)));
     }
 
     public void add(final Pawn pawn) {
