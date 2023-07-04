cf24842b6e450afe83b23f0e65ffd5d3b81793d0
Yunsik-Choi <choiys0311@gmail.com>
2023-07-04 13:09:29 +0900

    ⚡feat : 흰색 Pawn의 결과를 출력하는 기능 구현

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -2,10 +2,15 @@ package softeer2nd.chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import softeer2nd.chess.pieces.Pawn;
 
 public class Board {
     private final List<Pawn> pawns = new ArrayList<>();
+
+    public void initialize() {
+
+    }
 
     public void add(final Pawn pawn) {
         pawns.add(pawn);
@@ -20,5 +25,12 @@ public class Board {
 
     public int size() {
         return pawns.size();
+    }
+
+    public String getWhitePawnsResult() {
+        return pawns.stream()
+                .map(Pawn::getRepresentation)
+                .filter(representation -> representation.equals(Pawn.WHITE_REPRESENTATION))
+                .collect(Collectors.joining());
     }
 }
