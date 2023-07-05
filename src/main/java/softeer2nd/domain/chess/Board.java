1f81a979917e7ee5730ffc1ca1c7b90f3713ffa3
Yunsik-Choi <choiys0311@gmail.com>
2023-07-05 10:34:01 +0900

    ⚡feat : 체스판 출력 기능 구현

--- src/main/java/softeer2nd/domain/chess/Board.java
+++ src/main/java/softeer2nd/domain/chess/Board.java
@@ -4,14 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import softeer2nd.common.util.StringUtils;
 import softeer2nd.domain.chess.pieces.Pawn;
 import softeer2nd.domain.chess.pieces.Point;
 
 public class Board {
     public static final int WIDTH_SIZE = 8;
     public static final int HEIGHT_SIZE = 8;
-    private static final int WHITE_PAWN_Y = 1;
-    private static final int BLACK_PAWN_Y = 6;
+    private static final int WHITE_PAWN_Y = 6;
+    private static final int BLACK_PAWN_Y = 1;
+    private static final String BOARD_EMPTY_REPRESENTATION = ".";
 
     private final List<Pawn> pawns = new ArrayList<>();
 
@@ -57,5 +59,32 @@ public class Board {
 
     public List<Pawn> getPawns() {
         return pawns;
+    }
+
+    public String show() {
+        List<List<String>> result = initializeBoardRepresentation();
+
+        this.pawns.forEach(pawn -> setRepresentation(result, pawn));
+
+        return convertBoardRepresentation(result);
+    }
+
+    private void setRepresentation(final List<List<String>> result, final Pawn pawn) {
+        result.get(pawn.getPoint().getY())
+                .set(pawn.getPoint().getX(), pawn.getRepresentation());
+    }
+
+    private String convertBoardRepresentation(final List<List<String>> result) {
+        return result.stream()
+                .map(list -> String.join("", list))
+                .map(StringUtils::appendNewLine)
+                .collect(Collectors.joining());
+    }
+
+    private List<List<String>> initializeBoardRepresentation() {
+        return IntStream.range(0, Board.HEIGHT_SIZE)
+                .mapToObj(y -> IntStream.range(0, Board.WIDTH_SIZE)
+                        .mapToObj(x -> BOARD_EMPTY_REPRESENTATION).collect(Collectors.toList())
+                ).collect(Collectors.toList());
     }
 }
