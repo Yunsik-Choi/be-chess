a1ecf2356f57b2666dff60d9c1097aa33482aaca
Yunsik-Choi <choiys0311@gmail.com>
2023-07-05 12:10:08 +0900

    ⛑️ test : 행에 기물을 추가하는 기능 테스트 작성

--- src/test/java/softeer2nd/domain/chess/pieces/LineTest.java
+++ src/test/java/softeer2nd/domain/chess/pieces/LineTest.java
@@ -1,10 +1,13 @@
 package softeer2nd.domain.chess.pieces;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("체스판 행 관련 기능")
 class LineTest {
@@ -16,6 +19,32 @@ class LineTest {
         Assertions.assertAll(
                 () -> assertThat(line.getPieces()).containsOnlyNulls(),
                 () -> assertThat(line.getPieces()).hasSize(8)
+        );
+    }
+
+    @DisplayName("행에 기물을 추가한다.")
+    @ParameterizedTest(name = "index : {0}")
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7})
+    void set(int index) {
+        Line line = new Line();
+        Piece piece = new Piece(PieceColor.WHITE, PieceRepresentation.BLACK_REPRESENTATION);
+
+        line.set(index, piece);
+
+        assertThat(line.getPieces().get(index)).isEqualTo(piece);
+    }
+
+    @DisplayName("기물 추가시 행에 사이즈를 벗어나면 예외 처리한다.")
+    @Test
+    void setIndexOutOfSize() {
+        Line line = new Line();
+        Piece piece = new Piece(PieceColor.WHITE, PieceRepresentation.BLACK_REPRESENTATION);
+
+        Assertions.assertAll(
+                () -> assertThatThrownBy(() -> line.set(-1, piece))
+                        .isInstanceOf(IndexOutOfBoundsException.class),
+                () -> assertThatThrownBy(() -> line.set(Line.WIDTH, piece))
+                        .isInstanceOf(IndexOutOfBoundsException.class)
         );
     }
 }
