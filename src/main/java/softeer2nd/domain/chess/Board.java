4f3f5e0edabba3150421d163f543a85d74fc7242
Yunsik-Choi <choiys0311@gmail.com>
2023-07-05 12:19:28 +0900

    ♻️ refactor : 프로덕션에서 사용하지 않는 기능 제거

--- src/main/java/softeer2nd/domain/chess/Board.java
+++ src/main/java/softeer2nd/domain/chess/Board.java
@@ -5,15 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import softeer2nd.common.util.StringUtils;
+import softeer2nd.domain.chess.pieces.Line;
 import softeer2nd.domain.chess.pieces.Piece;
 import softeer2nd.domain.chess.pieces.PieceColor;
 import softeer2nd.domain.chess.pieces.PieceRepresentation;
 
 public class Board {
-    public static final int WIDTH_SIZE = 8;
     public static final int HEIGHT_SIZE = 8;
-    private static final int WHITE_Piece_Y = 6;
-    private static final int BLACK_Piece_Y = 1;
     private static final String BOARD_EMPTY_REPRESENTATION = ".";
 
     private final List<Piece> pieces = new ArrayList<>();
@@ -26,38 +24,8 @@ public class Board {
     }
 
     private void initializePiece(final PieceColor color, final PieceRepresentation representation) {
-        IntStream.range(0, WIDTH_SIZE)
+        IntStream.range(0, Line.WIDTH)
                 .forEach(x -> pieces.add(new Piece(color, representation)));
-    }
-
-    public void add(final Piece piece) {
-        pieces.add(piece);
-    }
-
-    public Piece findPiece(final int index) {
-        if (pieces.size() <= index) {
-            throw new IndexOutOfBoundsException("체스 판에 추가된 기물의 수보다 큽니다.");
-        }
-        return pieces.get(index);
-    }
-
-    public int size() {
-        return pieces.size();
-    }
-
-    public String getWhitePiecesResult() {
-        return getPiecesResultOwnedBy(PieceRepresentation.WHITE_REPRESENTATION);
-    }
-
-    public String getBlackPiecesResult() {
-        return getPiecesResultOwnedBy(PieceRepresentation.BLACK_REPRESENTATION);
-    }
-
-    private String getPiecesResultOwnedBy(final PieceRepresentation ownedRepresentation) {
-        return pieces.stream()
-                .map(Piece::getRepresentation)
-                .filter(representation -> representation.equals(ownedRepresentation.getValue()))
-                .collect(Collectors.joining());
     }
 
     public String show() {
@@ -75,7 +43,7 @@ public class Board {
 
     private List<List<String>> initializeBoardRepresentation() {
         return IntStream.range(0, Board.HEIGHT_SIZE)
-                .mapToObj(y -> IntStream.range(0, Board.WIDTH_SIZE)
+                .mapToObj(y -> IntStream.range(0, Line.WIDTH)
                         .mapToObj(x -> BOARD_EMPTY_REPRESENTATION).collect(Collectors.toList())
                 ).collect(Collectors.toList());
     }
--- src/test/java/softeer2nd/domain/chess/BoardTest.java
+++ src/test/java/softeer2nd/domain/chess/BoardTest.java
@@ -1,115 +1,18 @@
 package softeer2nd.domain.chess;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static softeer2nd.domain.chess.pieces.PieceRepresentation.BLACK_REPRESENTATION;
-import static softeer2nd.domain.chess.pieces.PieceRepresentation.WHITE_REPRESENTATION;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import softeer2nd.domain.chess.pieces.Piece;
-import softeer2nd.domain.chess.pieces.PieceColor;
 
 @DisplayName("체스 판 관련 기능")
 public class BoardTest {
     private Board board;
-    private Piece white;
-    private Piece black;
 
     @BeforeEach
     void setUp() {
         this.board = new Board();
-        this.white = new Piece(PieceColor.WHITE, WHITE_REPRESENTATION);
-        this.black = new Piece(PieceColor.BLACK, BLACK_REPRESENTATION);
-    }
-
-    @DisplayName("체스 판을 생성한다.")
-    @Test
-    public void create() {
-        board.add(white);
-        verifyAddPiece(board, 1, white, 0);
-
-        board.add(black);
-        verifyAddPiece(board, 2, black, 1);
-    }
-
-    @DisplayName("체스 판에 Piece을 추가한다.")
-    @Test
-    public void add() {
-        board.add(white);
-
-        verifyAddPiece(board, 1, white, 0);
-    }
-
-    @DisplayName("체스 판에서 Piece을 추가된 순번으로 가져온다.")
-    @Test
-    public void findPiece() {
-        board.add(white);
-        board.add(black);
-
-        assertAll(
-                () -> assertThat(board.findPiece(0)).isEqualTo(white),
-                () -> assertThat(board.findPiece(1)).isEqualTo(black)
-        );
-    }
-
-    @DisplayName("체스판의 기물 수보다 큰 순번으로 가져올 경우 예외 처리한다.")
-    @Test
-    void findPieceIndexOutOfBoundsException() {
-        board.add(white);
-
-        assertThatThrownBy(() -> board.findPiece(1)).isInstanceOf(IndexOutOfBoundsException.class);
-    }
-
-    @DisplayName("체스 판에 추가된 Piece 의 개수를 반환한다.")
-    @Test
-    void size() {
-        assertThat(board.size()).isZero();
-
-        board.add(white);
-
-        assertThat(board.size()).isOne();
-
-        board.add(black);
-
-        assertThat(board.size()).isEqualTo(2);
-    }
-
-    @DisplayName("체스판을 초기화한다.")
-    @Test
-    void initialize() {
-        board.initialize();
-
-        assertAll(
-                () -> assertEquals("pppppppp", board.getWhitePiecesResult()),
-                () -> assertEquals("PPPPPPPP", board.getBlackPiecesResult())
-        );
-    }
-
-    @DisplayName("흰색 Piece의 결과를 출력한다.")
-    @Test
-    void getWhitePieceResult() {
-        board.initialize();
-
-        assertEquals("pppppppp", board.getWhitePiecesResult());
-    }
-
-    @DisplayName("검은색 Piece의 결과를 출력한다.")
-    @Test
-    void getBlackPieceResult() {
-        board.initialize();
-
-        assertEquals("PPPPPPPP", board.getBlackPiecesResult());
-    }
-
-    private void verifyAddPiece(final Board board, final int boardSize, final Piece piece, final int PieceIndex) {
-        assertAll(
-                () -> assertThat(boardSize).isEqualTo(board.size()),
-                () -> assertThat(piece).isEqualTo(board.findPiece(PieceIndex))
-        );
     }
 
     @DisplayName("체스판을 출력한다.")
