f0d1879af9c250a2fc106cd9bbacd5f6658c7138
Yunsik-Choi <choiys0311@gmail.com>
2023-07-05 12:12:08 +0900

    ⚡feat : 행에 기물을 추가하는 기능 구현

--- src/main/java/softeer2nd/domain/chess/pieces/Line.java
+++ src/main/java/softeer2nd/domain/chess/pieces/Line.java
@@ -2,17 +2,30 @@ package softeer2nd.domain.chess.pieces;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Line {
+    public static final int WIDTH = 8;
+
     private final List<Piece> pieces = new ArrayList<>();
 
     public Line() {
-        for (int i = 0; i < 8; i++) {
-            pieces.add(null);
-        }
+        IntStream.range(0, WIDTH)
+                .forEach(i -> pieces.add(null));
     }
 
     public List<Piece> getPieces() {
         return this.pieces;
+    }
+
+    public void set(final int index, final Piece piece) {
+        if (isIndexOutOfBound(index)) {
+            throw new IndexOutOfBoundsException("행의 범위를 벗어났습니다.");
+        }
+        pieces.set(index, piece);
+    }
+
+    private static boolean isIndexOutOfBound(final int index) {
+        return index < 0 || index >= WIDTH;
     }
 }
