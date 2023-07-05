1f847aff23073be7d77a90e612b6564960133f98
Yunsik-Choi <choiys0311@gmail.com>
2023-07-05 13:46:03 +0900

    ♻️ refactor : 체스판 출력 로직 수정

--- src/main/java/softeer2nd/domain/chess/Board.java
+++ src/main/java/softeer2nd/domain/chess/Board.java
@@ -11,39 +11,67 @@ import softeer2nd.domain.chess.pieces.PieceRepresentation;
 
 public class Board {
     public static final int HEIGHT_SIZE = 8;
+
+    private static final int WHITE_PAWN_LINE_Y = 6;
+    private static final int BLACK_PAWN_LINE_Y = 1;
     private static final String BOARD_EMPTY_REPRESENTATION = ".";
 
-    private final List<Piece> pieces = new ArrayList<>();
+    private final List<Line> lines = new ArrayList<>();
 
     public void initialize() {
-        pieces.clear();
-
-        initializePiece(PieceColor.WHITE, PieceRepresentation.WHITE_REPRESENTATION);
-        initializePiece(PieceColor.BLACK, PieceRepresentation.BLACK_REPRESENTATION);
+        lines.clear();
+        IntStream.range(0, HEIGHT_SIZE).forEach(y -> lines.add(new Line()));
+        initializePiece(lines.get(WHITE_PAWN_LINE_Y), PieceColor.WHITE, PieceRepresentation.WHITE_REPRESENTATION);
+        initializePiece(lines.get(BLACK_PAWN_LINE_Y), PieceColor.BLACK, PieceRepresentation.BLACK_REPRESENTATION);
     }
 
-    private void initializePiece(final PieceColor color, final PieceRepresentation representation) {
+    private void initializePiece(
+            final Line line,
+            final PieceColor color,
+            final PieceRepresentation representation
+    ) {
         IntStream.range(0, Line.WIDTH)
-                .forEach(x -> pieces.add(new Piece(color, representation)));
+                .forEach(x -> line.set(x, new Piece(color, representation)));
     }
 
     public String show() {
         List<List<String>> result = initializeBoardRepresentation();
 
-        return convertBoardRepresentation(result);
-    }
+        setBoardRepresentation(result);
 
-    private String convertBoardRepresentation(final List<List<String>> result) {
-        return result.stream()
-                .map(list -> String.join("", list))
-                .map(StringUtils::appendNewLine)
-                .collect(Collectors.joining());
+        return boardRepresentationFormatting(result);
     }
 
     private List<List<String>> initializeBoardRepresentation() {
         return IntStream.range(0, Board.HEIGHT_SIZE)
                 .mapToObj(y -> IntStream.range(0, Line.WIDTH)
                         .mapToObj(x -> BOARD_EMPTY_REPRESENTATION).collect(Collectors.toList())
-                ).collect(Collectors.toList());
+                ).collect(Collectors.toUnmodifiableList());
+    }
+
+    private void setBoardRepresentation(final List<List<String>> result) {
+        IntStream.range(0, HEIGHT_SIZE)
+                .forEach(y -> setLineRepresentation(result, y));
+    }
+
+    private void setLineRepresentation(final List<List<String>> result, final int y) {
+        Line line = lines.get(y);
+
+        IntStream.range(0, Line.WIDTH)
+                .forEach(x -> result.get(y).set(x, getPieceRepresentation(line.getPieces().get(x))));
+    }
+
+    private String getPieceRepresentation(final Piece piece) {
+        if (piece == null) {
+            return BOARD_EMPTY_REPRESENTATION;
+        }
+        return piece.getRepresentation();
+    }
+
+    private String boardRepresentationFormatting(final List<List<String>> result) {
+        return result.stream()
+                .map(list -> String.join("", list))
+                .map(StringUtils::appendNewLine)
+                .collect(Collectors.joining());
     }
 }
