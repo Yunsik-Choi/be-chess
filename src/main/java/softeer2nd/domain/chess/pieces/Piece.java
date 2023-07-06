4353d16999bc9459cb5e2aae688a5143e58bc140
Yunsik-Choi <choiys0311@gmail.com>
2023-07-06 09:34:10 +0900

    ♻️ refactor : PieceColor Piece 내부에 구현

--- src/main/java/softeer2nd/domain/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/domain/chess/pieces/Piece.java
@@ -5,64 +5,68 @@ import static softeer2nd.domain.chess.pieces.PieceRepresentation.*;
 import java.util.Objects;
 
 public class Piece {
-    private final PieceColor color;
+    public enum Color {
+        WHITE, BLACK, NOCOLOR
+    }
+
+    private final Color color;
     private final PieceRepresentation representation;
 
-    private Piece(final PieceColor color, final PieceRepresentation representation) {
+    private Piece(final Color color, final PieceRepresentation representation) {
         this.color = color;
         this.representation = representation;
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(PieceColor.WHITE, WHITE_REPRESENTATION);
+        return new Piece(Color.WHITE, WHITE_REPRESENTATION);
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(PieceColor.BLACK, BLACK_REPRESENTATION);
+        return new Piece(Color.BLACK, BLACK_REPRESENTATION);
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(PieceColor.WHITE, WHITE_KNIGHT_REPRESENTATION);
+        return new Piece(Color.WHITE, WHITE_KNIGHT_REPRESENTATION);
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(PieceColor.BLACK, BLACK_KNIGHT_REPRESENTATION);
+        return new Piece(Color.BLACK, BLACK_KNIGHT_REPRESENTATION);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(PieceColor.WHITE, WHITE_ROOK_REPRESENTATION);
+        return new Piece(Color.WHITE, WHITE_ROOK_REPRESENTATION);
     }
 
     public static Piece createBlackRook() {
-        return new Piece(PieceColor.BLACK, BLACK_ROOK_REPRESENTATION);
+        return new Piece(Color.BLACK, BLACK_ROOK_REPRESENTATION);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(PieceColor.WHITE, WHITE_BISHOP_REPRESENTATION);
+        return new Piece(Color.WHITE, WHITE_BISHOP_REPRESENTATION);
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(PieceColor.BLACK, BLACK_BISHOP_REPRESENTATION);
+        return new Piece(Color.BLACK, BLACK_BISHOP_REPRESENTATION);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(PieceColor.WHITE, WHITE_QUEEN_REPRESENTATION);
+        return new Piece(Color.WHITE, WHITE_QUEEN_REPRESENTATION);
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(PieceColor.BLACK, BLACK_QUEEN_REPRESENTATION);
+        return new Piece(Color.BLACK, BLACK_QUEEN_REPRESENTATION);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(PieceColor.WHITE, WHITE_KING_REPRESENTATION);
+        return new Piece(Color.WHITE, WHITE_KING_REPRESENTATION);
     }
 
     public static Piece createBlackKing() {
-        return new Piece(PieceColor.BLACK, BLACK_KING_REPRESENTATION);
+        return new Piece(Color.BLACK, BLACK_KING_REPRESENTATION);
     }
 
-    public String getColor() {
-        return this.color.getValue();
+    public Color getColor() {
+        return this.color;
     }
 
     public String getRepresentation() {
@@ -70,11 +74,11 @@ public class Piece {
     }
 
     public boolean isWhite() {
-        return this.color.equals(PieceColor.WHITE);
+        return this.color.equals(Color.WHITE);
     }
 
     public boolean isBlack() {
-        return this.color.equals(PieceColor.BLACK);
+        return this.color.equals(Color.BLACK);
     }
 
     @Override
--- src/test/java/softeer2nd/domain/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/domain/chess/pieces/PieceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.ReflectionUtils;
+import softeer2nd.domain.chess.pieces.Piece.Color;
 
 @DisplayName("기물 관련 기능")
 public class PieceTest {
@@ -29,8 +30,8 @@ public class PieceTest {
     @DisplayName("생성자로 전달된 색상인 white 또는 black 으로 기물이 생성되어야 한다")
     @Test
     public void create() {
-        verifyPieceBy(white, PieceColor.WHITE.getValue(), WHITE_REPRESENTATION.getValue());
-        verifyPieceBy(black, PieceColor.BLACK.getValue(), BLACK_REPRESENTATION.getValue());
+        verifyPieceBy(white, Color.WHITE, WHITE_REPRESENTATION.getValue());
+        verifyPieceBy(black, Color.BLACK, BLACK_REPRESENTATION.getValue());
     }
 
     @DisplayName("기물의 출력 문자열을 반환한다.")
@@ -46,7 +47,7 @@ public class PieceTest {
     @Nested
     class createFactoryMethod {
         private Piece createPieceBy(
-                final PieceColor color,
+                final Color color,
                 final PieceRepresentation representation
         ) throws Exception {
             Constructor<Piece> declaredConstructor = ReflectionUtils.getDeclaredConstructor(Piece.class);
@@ -59,83 +60,83 @@ public class PieceTest {
         @DisplayName("흰색 폰을 생성한다.")
         @Test
         void createWhitePawn() throws Exception {
-            assertThat(Piece.createWhitePawn()).isEqualTo(createPieceBy(PieceColor.WHITE, WHITE_REPRESENTATION));
+            assertThat(Piece.createWhitePawn()).isEqualTo(createPieceBy(Color.WHITE, WHITE_REPRESENTATION));
         }
 
         @DisplayName("검은색 폰을 생성한다.")
         @Test
         void createBlackPawn() throws Exception {
-            assertThat(Piece.createBlackPawn()).isEqualTo(createPieceBy(PieceColor.BLACK, BLACK_REPRESENTATION));
+            assertThat(Piece.createBlackPawn()).isEqualTo(createPieceBy(Color.BLACK, BLACK_REPRESENTATION));
         }
 
         @DisplayName("흰색 나이트을 생성한다.")
         @Test
         void createWhiteKnight() throws Exception {
             assertThat(Piece.createWhiteKnight())
-                    .isEqualTo(createPieceBy(PieceColor.WHITE, WHITE_KNIGHT_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.WHITE, WHITE_KNIGHT_REPRESENTATION));
         }
 
         @DisplayName("검은색 나이트을 생성한다.")
         @Test
         void createBlackKnight() throws Exception {
             assertThat(Piece.createBlackKnight())
-                    .isEqualTo(createPieceBy(PieceColor.BLACK, BLACK_KNIGHT_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.BLACK, BLACK_KNIGHT_REPRESENTATION));
         }
 
         @DisplayName("흰색 룩을 생성한다.")
         @Test
         void createWhiteRook() throws Exception {
             assertThat(Piece.createWhiteRook())
-                    .isEqualTo(createPieceBy(PieceColor.WHITE, PieceRepresentation.WHITE_ROOK_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.WHITE, PieceRepresentation.WHITE_ROOK_REPRESENTATION));
         }
 
         @DisplayName("검은색 룩을 생성한다.")
         @Test
         void createBlackRook() throws Exception {
             assertThat(Piece.createBlackRook())
-                    .isEqualTo(createPieceBy(PieceColor.BLACK, PieceRepresentation.BLACK_ROOK_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.BLACK, PieceRepresentation.BLACK_ROOK_REPRESENTATION));
         }
 
         @DisplayName("흰색 비숍을 생성한다.")
         @Test
         void createWhiteBishop() throws Exception {
             assertThat(Piece.createWhiteBishop())
-                    .isEqualTo(createPieceBy(PieceColor.WHITE, PieceRepresentation.WHITE_BISHOP_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.WHITE, PieceRepresentation.WHITE_BISHOP_REPRESENTATION));
         }
 
         @DisplayName("검은색 비숍을 생성한다.")
         @Test
         void createBlackBishop() throws Exception {
             assertThat(Piece.createBlackBishop())
-                    .isEqualTo(createPieceBy(PieceColor.BLACK, PieceRepresentation.BLACK_BISHOP_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.BLACK, PieceRepresentation.BLACK_BISHOP_REPRESENTATION));
         }
 
         @DisplayName("흰색 퀸을 생성한다.")
         @Test
         void createWhiteQueen() throws Exception {
             assertThat(Piece.createWhiteQueen())
-                    .isEqualTo(createPieceBy(PieceColor.WHITE, PieceRepresentation.WHITE_QUEEN_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.WHITE, PieceRepresentation.WHITE_QUEEN_REPRESENTATION));
         }
 
         @DisplayName("검은색 퀸을 생성한다.")
         @Test
         void createBlackQueen() throws Exception {
             assertThat(Piece.createBlackQueen())
-                    .isEqualTo(createPieceBy(PieceColor.BLACK, PieceRepresentation.BLACK_QUEEN_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.BLACK, PieceRepresentation.BLACK_QUEEN_REPRESENTATION));
         }
 
         @DisplayName("흰색 킹을 생성한다.")
         @Test
         void createWhiteKing() throws Exception {
             assertThat(Piece.createWhiteKing())
-                    .isEqualTo(createPieceBy(PieceColor.WHITE, PieceRepresentation.WHITE_KING_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.WHITE, PieceRepresentation.WHITE_KING_REPRESENTATION));
         }
 
         @DisplayName("검은색 킹을 생성한다.")
         @Test
         void createBlackKing() throws Exception {
             assertThat(Piece.createBlackKing())
-                    .isEqualTo(createPieceBy(PieceColor.BLACK, PieceRepresentation.BLACK_KING_REPRESENTATION));
+                    .isEqualTo(createPieceBy(Color.BLACK, PieceRepresentation.BLACK_KING_REPRESENTATION));
         }
     }
 
@@ -153,7 +154,7 @@ public class PieceTest {
 
     private void verifyPieceBy(
             final Piece piece,
-            final String color,
+            final Color color,
             final String representation
     ) {
         assertAll(
