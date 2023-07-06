c039ab5988db8a3cff29cfcecda0abc997380f9c
Yunsik-Choi <choiys0311@gmail.com>
2023-07-06 10:57:25 +0900

    ⛑️ test : 기물의 색과 종류를 받아 개수를 반환하는 테스트 작성

--- src/test/java/softeer2nd/domain/chess/BoardTest.java
+++ src/test/java/softeer2nd/domain/chess/BoardTest.java
@@ -3,9 +3,12 @@ package softeer2nd.domain.chess;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static softeer2nd.common.util.StringUtils.appendNewLine;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import softeer2nd.domain.chess.pieces.Piece.Color;
+import softeer2nd.domain.chess.pieces.Piece.Type;
 
 @DisplayName("체스 판 관련 기능")
 public class BoardTest {
@@ -30,5 +33,18 @@ public class BoardTest {
                         appendNewLine("pppppppp") +
                         appendNewLine("rnbqkbnr"),
                 board.show());
+    }
+
+    @DisplayName("기물의 색과 종류를 받아 개수를 반환한다.")
+    @Test
+    void pieceCount() {
+        board.initialize();
+
+        Assertions.assertAll(
+                () -> assertEquals(8, board.pieceCount(Color.WHITE, Type.PAWN)),
+                () -> assertEquals(8, board.pieceCount(Color.BLACK, Type.PAWN)),
+                () -> assertEquals(2, board.pieceCount(Color.BLACK, Type.ROOK)),
+                () -> assertEquals(1, board.pieceCount(Color.BLACK, Type.QUEEN))
+        );
     }
 }
