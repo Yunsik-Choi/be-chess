e7cfcc98a4af515d47f212907f8cc4c4076ac43d
Yunsik-Choi <choiys0311@gmail.com>
2023-07-12 11:49:34 +0900

    ⚡feat : 룩 이동 구현

--- src/main/java/softeer2nd/domain/chess/pieces/Rook.java
+++ src/main/java/softeer2nd/domain/chess/pieces/Rook.java
@@ -1,6 +1,8 @@
 package softeer2nd.domain.chess.pieces;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Rook extends Piece {
     private static final String REPRESENTATION = "r";
@@ -25,6 +27,39 @@ public class Rook extends Piece {
 
     @Override
     public Piece move(final Position targetPosition, final List<List<Piece>> board) {
-        return new Rook(this.color, targetPosition, this.directions);
+        validationTargetPositionEqualCurrentPosition(targetPosition);
+
+        return this.getMovablePosition(position, board).stream()
+                .filter(position -> position.equals(targetPosition))
+                .findFirst()
+                .map(position -> new Rook(this.color, position, this.directions))
+                .orElseThrow(moveFailException());
+    }
+
+    @Override
+    protected List<Position> getMovablePosition(
+            final Position position,
+            final List<List<Piece>> board
+    ) {
+        return directions.stream()
+                .flatMap(direction -> getMovablePosition(position, direction, board).stream())
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private List<Position> getMovablePosition(
+            final Position position,
+            final Direction direction,
+            final List<List<Piece>> board
+    ) {
+        ArrayList<Position> result = new ArrayList<>();
+        if (!position.canMove(direction) || isSameColor(position.move(direction), board)) {
+            return result;
+        }
+        Position movePosition = position.move(direction);
+        result.add(movePosition);
+        if (isBlank(movePosition, board)) {
+            result.addAll(getMovablePosition(movePosition, direction, board));
+        }
+        return result;
     }
 }
